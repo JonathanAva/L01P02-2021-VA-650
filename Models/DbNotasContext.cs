aff583b1dcea3d133cd0526e301b3414d3a25062
Estudiante <Estudiante@PC01B45>
2024-04-06 13:05:24 -0600

    Agregar archivos de proyecto.

---
+++ Models/DbNotasContext.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2021_VA_650.Models;
+
+public partial class DbNotasContext : DbContext
+{
+    public DbNotasContext()
+    {
+    }
+
+    public DbNotasContext(DbContextOptions<DbNotasContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Alumno> Alumnos { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Facultade> Facultades { get; set; }
+
+    public virtual DbSet<Materia> Materias { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=PC01B45\\SQLEXPRESS; Database=db_notas; Trusted_Connection=TRUE; TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Alumno>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__alumnos__3213E83F51F46959");
+
+            entity.ToTable("alumnos", "dbo");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Apellidos)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("apellidos");
+            entity.Property(e => e.Codigo)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .HasColumnName("codigo");
+            entity.Property(e => e.Dui).HasColumnName("dui");
+            entity.Property(e => e.Estado).HasColumnName("estado");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__departam__3213E83FFD50A8F0");
+
+            entity.ToTable("departamentos", "dbo");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Departamento1)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("departamento");
+        });
+
+        modelBuilder.Entity<Facultade>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__facultad__3213E83FD96A9DBE");
+
+            entity.ToTable("facultades", "dbo");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Facultad)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("facultad");
+        });
+
+        modelBuilder.Entity<Materia>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__materias__3213E83FE5898623");
+
+            entity.ToTable("materias", "dbo");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Estado)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength()
+                .HasColumnName("estado");
+            entity.Property(e => e.Materia1)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("materia");
+            entity.Property(e => e.UnidadesValorativas).HasColumnName("unidades_valorativas");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/notasDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2021_VA_650.Models
+{
+    public class notasDbContext : DbContext
+    {
+        public notasDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
